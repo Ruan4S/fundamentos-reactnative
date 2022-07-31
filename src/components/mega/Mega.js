6fab323c517d74cd8f604af6d18c21bc4765f049
Ruan Araújo <ruands2002@gmail.com>
2022-07-31 00:49:37 -0300

    feat(Componente de Classe): cuidados com o 'this'

--- src/components/mega/Mega.js
+++ src/components/mega/Mega.js
@@ -1,9 +1,38 @@
 import { Component } from "react";
-import { Text } from "react-native";
+import { Text, TextInput } from "react-native";
 import Estilo from "../../styles/Estilo";
 
 export default class Mega extends Component {
+  state = {
+    qtdNumeros: this.props.qtdNumeros,
+  };
+
+  alterarQtdNumero = (qtd) => {
+    this.setState({ qtdNumeros: qtd });
+  };
+
+  /*   constructor(props) {
+    super(props);
+
+    this.alterarQtdNumero = this.alterarQtdNumero.bind(this);
+
+    this.state = {
+      qtdNumeros: props.qtdNumeros,
+    };
+  } */
+
   render() {
-    return <Text style={Estilo.txtG}>Gerador de Mega-Sena {this.props.qtdNumeros}</Text>;
+    return (
+      <>
+        <Text style={Estilo.txtG}>Gerador de Mega-sena {this.state.qtdNumeros}</Text>
+        <TextInput
+          keyboardType="numeric"
+          style={{ borderBottomWidth: 1 }}
+          placeholder="Qtde de Números"
+          value={this.state.qtdNumeros}
+          onChangeText={this.alterarQtdNumero}
+        />
+      </>
+    );
   }
 }
